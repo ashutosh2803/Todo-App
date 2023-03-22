2735a0ff7debbbfd7355b3125e71636e58371acf
unknown <ashuisalive@gmail.com>
2023-03-22 12:21:53 +0530

    fixed css issues in mobile app

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -4,12 +4,12 @@ import { Button } from "react-bootstrap";
 const TaskItem = ({ title, handleToggle, handleDelete, status, id }) => {
   return (
     <div className="d-flex justify-content-between align-items-center p-2">
-      <div className="display-6" style={{letterSpacing: '0.1ch'}}>{title}</div>
+      <div style={{letterSpacing: '0.1ch', fontSize: '20px', fontWeight: '500'}}>{title}</div>
       <div>
-        <Button style={{letterSpacing: '0.1ch'}} className={status ? 'btn btn-success mx-2' : 'btn btn-warning mx-2'} onClick={() => handleToggle(id)}>
+        <Button className={status ? 'btn btn-success mx-1' : 'btn btn-warning mx-1'} onClick={() => handleToggle(id)}>
           {status ? `COMPLETED` : `PENDING`}
         </Button>
-        <Button style={{letterSpacing: '0.1ch'}} className="btn btn-danger mx-2" onClick={() => handleDelete(id)}>
+        <Button className="btn btn-danger mx-1" onClick={() => handleDelete(id)}>
           DELETE
         </Button>
       </div>
